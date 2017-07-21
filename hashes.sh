27c5f0eda6697a4952022e7f64022c85f6cd90c1
Tahina Ramananandro <taramana@microsoft.com>
2017-07-21 09:55:29 -0700

    Upgrade miTLS to latest master

--- hashes.sh
+++ hashes.sh
@@ -1,9 +1,9 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=0f36db85994c7a6129547880775b8caef5767548
-branches[mitls-fstar]=master
 hashes[vale]=f6c33caf1ffc8291e7baaace8efe2e4a4e741926
 branches[vale]=master
+hashes[mitls-fstar]=4a28acc2ef491a560b779e647e89e600ea6ea178
+branches[mitls-fstar]=master
 hashes[FStar]=3651efd269c50a7b25eadb66cefddda7cdf1678f
 branches[FStar]=taramana_import_vale_asm
 hashes[hacl-star]=c01836a18db779993432cbe5b9891bee025138b1
