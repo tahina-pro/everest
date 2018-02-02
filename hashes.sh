eb2d630cffdae4c97db794951039ee5ffda5ab42
Tahina Ramananandro <taramana@microsoft.com>
2018-02-02 10:34:20 -0800

    Replace fprintf(stderr, ...) with a macro (KRML_HOST_EPRINTF)

--- hashes.sh
+++ hashes.sh
@@ -1,7 +1,7 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=3b6f4d4609310cb83afb89bcccc4998588dce6a0
-branches[mitls-fstar]=quic2c
+hashes[mitls-fstar]=cfab7a48206c61fa716d447e1737a9864d4de24d
+branches[mitls-fstar]=taramana_eprintf
 hashes[vale]=777e2cfd03e8868c98fa953da34b156e10dd8baf
 branches[vale]=master
 hashes[FStar]=bf6e23954e078c8b90a9085deb2ee908d062624d
@@ -10,5 +10,5 @@ hashes[MLCrypto]=9a4d0cee16f4b62ca02f35445245d3d382f86a53
 branches[MLCrypto]=master
 hashes[hacl-star]=76cbc9fd489224e65030bc64fa187086703b5163
 branches[hacl-star]=c_mitls2c
-hashes[kremlin]=590967f8a7a1b3568cdfa460f5f9c6358f6b813a
-branches[kremlin]=fstar-master
+hashes[kremlin]=31c00160307543ef031ae38939f3ece89b5c62e0
+branches[kremlin]=taramana_eprintf
