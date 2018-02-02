520dc2571c31fe1cdffebad3f10cd608520c1ea7
Tahina Ramananandro <taramana@microsoft.com>
2018-02-02 12:57:03 -0800

    Update

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=cfab7a48206c61fa716d447e1737a9864d4de24d
+hashes[mitls-fstar]=fb5433589d21d70eda2f07c07ebdf2cc7bd4131b
 branches[mitls-fstar]=taramana_eprintf
 hashes[vale]=777e2cfd03e8868c98fa953da34b156e10dd8baf
 branches[vale]=master
-hashes[FStar]=bf6e23954e078c8b90a9085deb2ee908d062624d
+hashes[FStar]=5c95e776535ac878c56f09cb2752e7eb9e7ce2f5
 branches[FStar]=master
 hashes[MLCrypto]=9a4d0cee16f4b62ca02f35445245d3d382f86a53
 branches[MLCrypto]=master
-hashes[hacl-star]=76cbc9fd489224e65030bc64fa187086703b5163
+hashes[hacl-star]=601b9911e2fb34df308ba981b78be499734789e1
 branches[hacl-star]=c_mitls2c
-hashes[kremlin]=31c00160307543ef031ae38939f3ece89b5c62e0
+hashes[kremlin]=e61c0b64f7ce85f59621b172f43f17bac6d00ec8
 branches[kremlin]=taramana_eprintf
